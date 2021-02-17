73fac4888711fd01b349c2ae61c34322150060b9
Esraa350 <aesraa350@yahoo.com>
2021-02-17 03:28:37 +0200

    editlogin

---
+++ xo/src/secscene/scenetwocontroller.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secscene;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author esraa abou alkassem
+ */
+public class scenetwocontroller implements Initializable {
+
+    @FXML
+    private AnchorPane bp;
+    @FXML
+    private RadioButton friend;
+    @FXML
+    private ToggleGroup group;
+    @FXML
+    private RadioButton pc;
+    @FXML
+    private Button register;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
