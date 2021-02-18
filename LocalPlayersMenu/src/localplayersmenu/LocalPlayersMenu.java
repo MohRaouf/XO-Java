031d035524bff2061b76e37979d5c3131019f058
MohamedBassiouny26 <mohamedsharf26797@gmail.com>
2021-02-18 13:34:49 +0200

    undecorated

--- LocalPlayersMenu/src/localplayersmenu/LocalPlayersMenu.java
+++ LocalPlayersMenu/src/localplayersmenu/LocalPlayersMenu.java
@@ -10,6 +10,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -22,7 +23,7 @@ public class LocalPlayersMenu extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
