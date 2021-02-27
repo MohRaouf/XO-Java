c1b9d73ebe769798243a60b2cca0af3bac27cc08
=Aalaa Habib <aalaahabib.ah@gmail.com>
2021-02-28 01:06:39 +0200

    update Design

--- DesignPatten/src/singleResponsibilty/women.java
+++
@@ -1,5 +0,0 @@
-package singleResponsibilty;
-
-public class women {
-    
-}
--- DesignPatten/src/singleton/Main.java
+++
@@ -1,9 +0,0 @@
-package singleton;
-
-public class Main {
-    
-    public static void main(String[] args) {
-
-            openConnection serverCon = openConnection.getInstance();
-    }
-}
